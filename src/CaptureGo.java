eafe5c8a143f34c3810cdeaea4f93529fc14a8a9
LENART MIKLAVIC <miklavic.lenart@gmail.com>
2023-05-25 18:18:11 +0200

    Metoda main za poganjanje igre.

---
+++ src/CaptureGo.java
@@ -0,0 +1,13 @@
+// Vsebuje metodo main. Za zaganjanje igre.
+
+import gui.Okno;
+import vodja.Vodja;
+
+public class CaptureGo {
+    public static void main(String[] args) {
+        Okno okno = new Okno();
+        okno.pack();
+        okno.setVisible(true);
+        Vodja.okno = okno;
+    }
+}
